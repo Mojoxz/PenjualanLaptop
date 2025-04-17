bc77ba5d2979a9d6f04b4fce4ce1ee59306cc5cc
Firmandhani <dhanimojo01@gmail.com>
2025-04-17 14:01:00 +0700

    perubahan 17 april 2.0

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $brands = query("SELECT * FROM tb_merk ORDER BY nama_merk ASC");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Toko Laptop - Pusat Penjualan Laptop Terpercaya</title>
+    <title>Unesa Laptop - Pusat Penjualan Laptop Terpercaya</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <!-- Tambahkan Animate.css untuk animasi -->
